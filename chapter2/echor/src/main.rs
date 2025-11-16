12d8fde2853f3fcc978f7a25aa3cd1addbde3cee
fract <87593133+inoUwU@users.noreply.github.com>
2025-11-16 23:56:53 +0900

    wip

--- chapter2/echor/src/main.rs
+++ chapter2/echor/src/main.rs
@@ -1,3 +1,35 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("{:?}", std::env::args())
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Fract")
+        .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1), // 1つ以上指定
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    /* TODO: Updateで戻り値が変わったかも
+    本家のリポジトリを確認してみる */
+    let text = matches.value_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+    // println!("{:#?}", matches)
+    let text_vec = [text.into_owned()];
+
+    println!(
+        "{}{}",
+        text_vec.join(" "),
+        if omit_newline { "" } else { "\n" }
+    );
 }
